c9e6c4267389148c149b970297e637c1671ffa14
fadillaoegi <fadilfdl2710@gmail.com>
2024-04-30 10:36:17 +0700

    save

---
+++ lib/modules/prsentations/screens/auth/sigin_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+      ),
+    );
+  }
+}
---
+++ lib/modules/prsentations/screens/auth/signup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+      ),
+    );
+  }
+}
