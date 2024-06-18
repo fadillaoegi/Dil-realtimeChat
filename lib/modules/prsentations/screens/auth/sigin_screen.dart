88daa59aefd06cb9fee6224a684d35b215daa14a
Fadil <fadilfdl2710@gmail.com>
2024-06-18 11:46:48 +0700

    save

--- lib/modules/prsentations/screens/auth/sigin_screen.dart
+++ lib/modules/prsentations/screens/auth/sigin_screen.dart
@@ -1,65 +1,82 @@
-
+import 'package:dilchat/modules/prsentations/controllers/signin_controller.dart';
+import 'package:dilchat/themes/fonts.dart';
 import 'package:dilchat/widgets/form_apps_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends GetView<SignInController> {
   const SignInScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: Get.height,
-        width: Get.width,
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: Get.width / 1.2,
-              height: 400.0,
-              decoration: BoxDecoration(
-                  boxShadow: const [],
-                  color: Colors.blueAccent,
-                  // color: ColorApps.primary,
-                  borderRadius: BorderRadius.circular(10.0)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const FormApps(
-                    hint: "hint",
-                    label: "label",
+    final signInC = Get.put(SignInController());
+    return Obx(
+      () => GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          body: Container(
+            height: Get.height,
+            width: Get.width,
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: Get.width / 1.2,
+                  height: Get.height / 1.9,
+                  decoration: BoxDecoration(
+                      boxShadow: const [],
+                      color: Colors.blueAccent,
+                      // color: ColorApps.primary,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 40.0),
+                        child: Text(
+                          "Sign In",
+                          style: white500.copyWith(fontSize: 34.0),
+                        ),
+                      ),
+                      const FormApps(
+                        label: "Email",
+                        hint: "example@gmail.com",
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      FormApps(
+                        label: "Password",
+                        obscureText: signInC.obsecureText.value,
+                        hint: "",
+                        suffixActive: true,
+                        onTap: signInC.seePassword,
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        width: Get.width,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(),
+                          child: const Text("Sign In"),
+                        ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  const FormApps(
-                    hint: "hint",
-                    label: "label",
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    width: Get.width,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(),
-                      child: const Text("data"),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 }
-
-
--- lib/widgets/form_apps_widget.dart
+++ lib/widgets/form_apps_widget.dart
@@ -1,15 +1,22 @@
 import 'package:dilchat/themes/fonts.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FormApps extends StatelessWidget {
   final TextEditingController? controller;
   final String? hint;
   final String? label;
+  final bool? obscureText;
+  final bool? suffixActive;
+  final VoidCallback? onTap;
   const FormApps({
     super.key,
     this.controller,
-    this.hint,
-    this.label,
+    this.obscureText = false,
+    this.hint = "",
+    this.label = "",
+    this.suffixActive = false,
+    this.onTap,
   });
 
   @override
@@ -24,14 +31,31 @@ class FormApps extends StatelessWidget {
             padding: const EdgeInsets.only(bottom: 10.0),
             child: Text(
               label!,
-              style: white600.copyWith(fontSize: 16.0),
+              style: white500.copyWith(fontSize: 16.0),
             ),
           ),
           TextFormField(
             cursorColor: Colors.white,
             controller: controller,
+            obscureText: obscureText!,
+            style: const TextStyle(color: Colors.white),
             decoration: InputDecoration(
+              hintStyle: const TextStyle(color: Colors.white60),
               hintText: hint,
+              suffix: suffixActive!
+                  ? InkWell(
+                      onTap: onTap,
+                      child: obscureText!
+                          ? const Icon(
+                              CupertinoIcons.eye,
+                              color: Colors.white,
+                            )
+                          : const Icon(
+                              CupertinoIcons.eye_slash,
+                              color: Colors.white,
+                            ),
+                    )
+                  : const SizedBox(),
               enabledBorder: const OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(40.0)),
                 borderSide: BorderSide(
