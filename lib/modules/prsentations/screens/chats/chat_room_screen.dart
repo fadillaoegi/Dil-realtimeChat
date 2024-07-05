6f6ff315ae946316ac6de48bdd929a655f81db9d
Fadil <fadilfdl2710@gmail.com>
2024-07-05 10:03:04 +0700

    update

--- lib/modules/prsentations/screens/chats/chat_room_screen.dart
+++ lib/modules/prsentations/screens/chats/chat_room_screen.dart
@@ -1,10 +1,152 @@
+import 'package:dilchat/themes/colors.dart';
+import 'package:dilchat/themes/fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ChatRoomScreen extends StatelessWidget {
   const ChatRoomScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: ColorApps.secondary,
+      appBar: AppBar(
+        backgroundColor: ColorApps.secondary,
+        leading: InkWell(
+          onTap: () => Get.back(),
+          child: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(
+          "NameUserToChat",
+          style: white600.copyWith(fontSize: 20.0),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            ChatBox(),
+            SizedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    width: Get.width * 0.6,
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: ColorApps.primary.withOpacity(0.5),
+                            spreadRadius: 0,
+                            blurRadius: 7,
+                            offset: const Offset(0, 5),
+                          )
+                        ],
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                        )),
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "PING !",
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Container(
+                    height: 36.0,
+                    width: 36.0,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(100.0)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: ColorApps.primary.withOpacity(0.5),
+                            spreadRadius: 0,
+                            blurRadius: 7,
+                            offset: const Offset(0, 5),
+                          )
+                        ]),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatBox extends StatelessWidget {
+  const ChatBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 20.0),
+          child: Row(
+            children: [
+              Container(
+                height: 36.0,
+                width: 36.0,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        const BorderRadius.all(Radius.circular(100.0)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: ColorApps.primary.withOpacity(0.5),
+                        spreadRadius: 0,
+                        blurRadius: 7,
+                        offset: const Offset(0, 5),
+                      )
+                    ]),
+              ),
+              const SizedBox(
+                width: 10.0,
+              ),
+              Container(
+                width: Get.width * 0.6,
+                padding: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: ColorApps.primary.withOpacity(0.5),
+                      spreadRadius: 0,
+                      blurRadius: 7,
+                      offset: const Offset(0, 5),
+                    )
+                  ],
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(10),
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                    "PING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
