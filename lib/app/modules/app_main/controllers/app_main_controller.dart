043c1abfd7f4af59ffa95549229e36e085f1c8e9
fadillaoegi <fadilfdl2710@gmail.com>
2025-04-27 23:42:32 +0700

    just cendol

--- lib/app/modules/app_main/controllers/app_main_controller.dart
+++ lib/app/modules/app_main/controllers/app_main_controller.dart
@@ -1,23 +1,10 @@
-import 'package:get/get.dart';
+import 'package:get/get_rx/get_rx.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class AppMainController extends GetxController {
-  //TODO: Implement AppMainController
-
   final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
+  
 
   void increment() => count.value++;
 }
